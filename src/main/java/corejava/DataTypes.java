250a2f4694c6948b70831cc7967c160b69352e00
Archana Deulkar <88867952+sawantarchna@users.noreply.github.com>
2022-12-14 12:34:00 -0500

    first commit

---
+++ src/main/java/corejava/DataTypes.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int myNum = 5;//primitive datatype
+		String website = "Rahul Shetty academy";//non primitive
+		 char letter = 'r';
+		 double dec = 5.99;
+		 boolean myCard = true;
+		
+		System.out.println(myNum);
+		
+		
+
+	}
+
+}
